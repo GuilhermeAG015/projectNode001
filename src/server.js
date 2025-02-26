d29bfca37fbdf534e855a7fcec0f26aaa5fca59e
Guilherme Alexandre <guilhermegrilo2009@hotmail.com>
2025-02-26 20:04:56 -0300

    Adding Status code

--- src/server.js
+++ src/server.js
@@ -18,10 +18,12 @@ const server = http.createServer((req, res) => {
       name: 'John Doe',
       email: 'johndoe@example.com'
     })
-    return res.end('criação de usuário')
+    return res
+      .writeHead(201)
+      .end()
   }
 
-  return res.end('Hello world')
+  return res.writeHead(404).end('Not Found')
 })
 
 server.listen(3001)
