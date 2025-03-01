6f0ee355df66904d8ae1498d105f28fdfe900fe6
Guilherme Alexandre <guilhermegrilo2009@hotmail.com>
2025-03-01 18:09:36 -0300

    Introduction to streams

---
+++ streams/fundamentals.js
@@ -0,0 +1,26 @@
+// Streams -->
+
+// process.stdin
+//   .pipe(process.stdout)
+
+import { Readable } from 'node:stream'
+
+class OneToHundredStram extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
+
+new OneToHundredStram()
+  .pipe(process.stdout)
