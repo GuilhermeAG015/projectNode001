d775ae6436f2aa9e21fcc6da68ef1edd39d7013e
Guilherme Alexandre <guilhermegrilo2009@hotmail.com>
2025-03-03 15:44:08 -0300

    Adding Database file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
-const users = []
+const database = new Database
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -9,18 +10,20 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
-    users.push({
+    const user = {
       id: 1,
       name,
       email
-    })
+    }
+
+    database.insert('users', user)
+
     return res
       .writeHead(201)
       .end()
@@ -29,4 +32,6 @@ const server = http.createServer(async (req, res) => {
   return res.writeHead(404).end('Not Found')
 })
 
-server.listen(3001)
+server.listen(3001, () => {
+  console.info('Server is running on port 3001')
+})
